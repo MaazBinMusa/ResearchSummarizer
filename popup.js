73841f9f71bbd11eda9e032354654be9795b7155
maaz <maazbin-musa@uiowa.edu>
2023-09-26 16:18:32 -0500

    made it just a title extension

--- popup.js
+++ popup.js
@@ -13,23 +13,13 @@ chrome.runtime.sendMessage({greeting: "GetText"},
             if(data.hasOwnProperty(taburl)){
                 console.log("API has the url");
                 console.log(data);
-                let summary = data[taburl]['summary'];
-                let title = data[taburl]['title'];
-                setsummaryhtml(summary,title);
+                let title = data[taburl]['Title'];
+                setsummaryhtml(title);
                 return true;
             }
             else{
-                geturldata(taburl)
-                .then((data) => {
-                    // Add to storage
-                    if(data.error === false){
-                        let setdatapi = apiurl + "add";
-                        setapidata(setdatapi,data,taburl);
-                    }
-                    else{
-                        document.getElementById("content").innerHTML = "<div id='content'> <h2 class='heading'> Error: Not a pdf </h2> </div>";
-                    }
-                });
+                setdatapiurl = apiurl + "add";
+                setapidata(setdatapiurl,taburl);
             }
         });
 
@@ -39,7 +29,7 @@ chrome.runtime.sendMessage({greeting: "GetText"},
 
 );
 
-async function setapidata(url,data,taburl){
+async function setapidata(url,taburl){
 
     let apidata = await fetch(url, {
         method: 'POST',
@@ -48,66 +38,22 @@ async function setapidata(url,data,taburl){
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            name:taburl,
-            pagetext:data.pagetext, 
-            npages:data.npages,
-            title:data.title
+            name:taburl
         })
     });
 
     apidata = await apidata.json();
-    let summary = apidata[taburl]['summary'];
-    let title = apidata[taburl]['title'];
-    await setsummaryhtml(summary,title);
 
-    console.log("Data added to API");
-
-    return true;
-};
-
-async function geturldata(url){
-
-    // check if url is undefined // check if the url is a pdf
-    if(url === undefined || url === null || !(url.includes(".pdf"))){
-        return {pagetext:'-1', error: true};
+    if(apidata.hasOwnProperty(taburl)){
+       let title = apidata[taburl]['Title'];
+        await setsummaryhtml(title);
+        console.log("Data added to API");
     }
     else{
-        let pagedata = await getpagedata(url);
-        return {
-            pagetext : pagedata.text, 
-            npages: pagedata.npages,
-            title: pagedata.title, 
-            error: false
-        };
-    }
-};
-
-async function getpagedata(pdfUrl){
-    
-    pdfjsLib.GlobalWorkerOptions.workerSrc = './pdfjs-3.10.111-dist/build/pdf.worker.js';
-    const loadingTask = pdfjsLib.getDocument(pdfUrl);
-    const pdf = await loadingTask.promise;
-    const numPages = pdf.numPages;
-
-    const md = await pdf.getMetadata();
-    console.log(md);
-
-    // Fetch the text from all the pages
-    let _text = '';
-    for(let i=1; i<=numPages; i++){
-        const page = await pdf.getPage(i);
-        const textContent = await page.getTextContent();
-        const text = textContent.items.map(function (s) {
-            return s.str;
-        }).join(' ');
-        _text += text;
+        console.log("Data not added to API");
     }
 
-    return {
-        text: _text, 
-        npages: numPages,
-        title: md.info.Title
-    }; 
+    return true;
 };
 
 async function makesummaryhtml(summary){
@@ -117,17 +63,9 @@ async function makesummaryhtml(summary){
     return summaryhtml;
 };
 
-async function setsummaryhtml(summary,title){
+async function setsummaryhtml(title){
 
-    let summaryhtml = await makesummaryhtml(summary);
-
-    console.log(summaryhtml);
-
-    customhtml = "<h2 class='heading'>"+title+"</h2>";
-
-    for (let i = 0; i < summaryhtml.length; i++) {
-         customhtml += "<p class='bubble'>"+summaryhtml[i]+"</p>";
-    }
+    const customhtml = "<h2 class='heading'>"+title+"</h2>";
 
     document.getElementById("content").innerHTML = customhtml;
 
